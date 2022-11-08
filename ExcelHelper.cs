9575421773ec84b506667caa0b035d78f73dc038
maksi <maksi@TESLENKO-MAXIM>
2022-11-08 05:25:37 +0300

    Добавьте файлы проекта.

---
+++ ExcelHelper.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Project1
+{
+    internal class ExcelHelper: IDisposable
+    {
+        private Application _excel;
+        private Workbook _workbook;
+        private string _filePath;
+
+        public ExcelHelper() 
+        {
+            _excel = new Excel.Application();
+        }
+
+        internal bool Open(string filePath)
+        {
+            try
+            {
+                if (File.Exists(filePath))
+                {
+                    _workbook = _excel.Workbooks.Open(filePath);
+                }
+                else
+                {
+                    Console.WriteLine("Файла не существует");
+                }
+                return true;
+            }
+            catch(Exception ex) { Console.WriteLine(ex.Message); }
+            return false;
+        }
+
+        internal bool Set(string column, int row, object data)
+        {
+            try
+            {
+                (_excel.ActiveSheet as Excel.Worksheet).Cells[row, column] = data;
+                return true;
+            }
+            catch(Exception ex) { Console.WriteLine(ex.Message); }
+            return false; 
+        }
+
+        internal void Save()
+        {
+            if (!string.IsNullOrEmpty(_filePath))
+            {
+                _workbook.SaveAs(_filePath);
+            }
+            else
+            {
+                _workbook.Save();
+            }
+        }
+
+        internal bool Get(int sheetNum, List<string> listNeededJpgs, string currentSheet)
+        {
+            Excel.Worksheet ObjWorkSheet;
+            try
+            {
+                ObjWorkSheet = (Excel.Worksheet)_workbook.Sheets[sheetNum];
+                currentSheet = ObjWorkSheet.Name;
+                /*как-то надо выбрать правильный столбец и получить номера*/
+                return true;
+            }
+            catch(Exception ex) { Console.WriteLine(ex.Message); }
+            return false;
+        }
+        public void Dispose()
+        {
+            try
+            {
+                _workbook.Close();
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PdfCreate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string catalog = Environment.CurrentDirectory, currentSheet = null;
+            int i = 0;
+            List<string> listNeededJpgs = new List<string>();
+            List<List<string>> listOfPJpgs = new List<List<string>>();
+
+            foreach (string findedFile in Directory.EnumerateFiles(catalog, "*.jpg*", SearchOption.AllDirectories))
+            {
+                FileInfo FI;
+                try
+                {
+                    FI = new FileInfo(findedFile);
+                    listOfPJpgs.Add(new List<string>());
+                    listOfPJpgs[i][0] = FI.Name;
+                    listOfPJpgs[i][1] = FI.FullName;
+                    i++;
+                }
+                catch (Exception ex) { Console.WriteLine(ex.Message); }
+            }
+
+            try
+            {
+                using (Project1.ExcelHelper helper = new Project1.ExcelHelper())
+                {
+                    if (helper.Open(filePath: Path.Combine(catalog, "Test.xlsx")))
+                    {
+                        i = 1;
+                        while (true) 
+                        {
+                            try
+                            {
+                                helper.Get(sheetNum: i, listNeededJpgs, currentSheet);
+                            }
+                            catch(Exception ex) { Console.WriteLine(ex.Message); break; }
+                        }
+                        helper.Save();
+                    }
+                }
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+        }
+    }
+}
